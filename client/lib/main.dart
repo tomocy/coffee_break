31c923730e18f818b816993a14fb731f09b23b9d
tomocy <tomocy.dev@gmail.com>
2020-01-04 18:27:29 +0900

    add Page
    Page extends a page which shows the list of links fetched by the given fetch function.

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -6,21 +6,60 @@ class App extends StatelessWidget {
   const App({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) =>
-      const MaterialApp(home: HomePage(title: 'Home'));
+  Widget build(BuildContext context) => MaterialApp(
+        home: Page(
+          title: 'Home',
+          fetch: () async {
+            return Future.delayed(
+              const Duration(seconds: 1),
+              () => List.generate(100, (i) => 'List $i'),
+            );
+          },
+        ),
+      );
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({
+class Page extends StatefulWidget {
+  const Page({
     Key key,
     @required this.title,
+    @required this.fetch,
   })  : assert(title != null),
+        assert(fetch != null),
         super(key: key);
 
   final String title;
+  final Future<List<String>> Function() fetch;
+
+  @override
+  _PageState createState() => _PageState();
+}
+
+class _PageState extends State<Page> {
+  Future<List<String>> _links;
+
+  @override
+  void initState() {
+    super.initState();
+    _links = widget.fetch();
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: Text(title)),
+        appBar: AppBar(title: Text(widget.title)),
+        body: FutureBuilder<List<String>>(
+          future: _links,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, i) =>
+                    ListTile(title: Text(snapshot.data[i])),
+              );
+            }
+
+            return const Center(child: CircularProgressIndicator());
+          },
+        ),
       );
 }
