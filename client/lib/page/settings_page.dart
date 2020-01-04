88fed1e953c3fdcfa12690da345e9951daf0cf06
tomocy <tomocy.dev@gmail.com>
2020-01-05 01:55:19 +0900

    add SettingsPages
    SettingsPages show the current settings.

---
+++ client/lib/page/settings_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+const _themes = <ThemeMode, String>{
+  ThemeMode.system: 'System',
+  ThemeMode.dark: 'Dark',
+  ThemeMode.light: 'Light',
+};
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Consumer<Settings>(
+        builder: (context, settings, child) => ListView(children: <Widget>[
+          SettingsListTile(
+            title: 'Theme',
+            onChanged: (theme) => settings.themeMode =
+                _themes.entries.firstWhere((entry) => entry.value == theme).key,
+            selectedItem: _themes[settings.themeMode],
+            items: _themes.values.toList(),
+          )
+        ]),
+      );
+}
+
+class SettingsListTile extends StatefulWidget {
+  const SettingsListTile({
+    Key key,
+    this.title,
+    this.onChanged,
+    this.selectedItem,
+    this.items,
+  }) : super(key: key);
+
+  final String title;
+  final void Function(String) onChanged;
+  final String selectedItem;
+  final List<String> items;
+
+  @override
+  _SettingsListTileState createState() => _SettingsListTileState();
+}
+
+class _SettingsListTileState extends State<SettingsListTile> {
+  String _selectedItem;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedItem = widget.selectedItem;
+  }
+
+  @override
+  Widget build(BuildContext context) => ListTile(
+        title: Text(widget.title),
+        trailing: DropdownButton<String>(
+          value: _selectedItem,
+          onChanged: (item) {
+            setState(() => _selectedItem = item);
+            widget.onChanged(item);
+          },
+          items: widget.items
+              .map((item) => DropdownMenuItem<String>(
+                    value: item,
+                    child: Text(item),
+                  ))
+              .toList(),
+        ),
+      );
+}
+
+class Settings extends ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  ThemeMode get themeMode => _themeMode;
+  set themeMode(ThemeMode mode) {
+    _themeMode = mode;
+    notifyListeners();
+  }
+}
