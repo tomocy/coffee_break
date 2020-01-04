83c19646cfa51941e6958c854241a9f0cac997ba
tomocy <tomocy.dev@gmail.com>
2020-01-04 20:15:15 +0900

    add LinksListView
    LinksListView shows the list of the links with LinksRepository.

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(const App());
 
@@ -12,14 +14,16 @@ class App extends StatelessWidget {
           fetch: () async {
             return Future.delayed(
               const Duration(seconds: 1),
-              () => List.generate(100, (i) => 'List $i'),
+              () => Random().nextBool()
+                  ? List.generate(100, (i) => 'List $i')
+                  : throw FetchException(),
             );
           },
         ),
       );
 }
 
-class Page extends StatefulWidget {
+class Page extends StatelessWidget {
   const Page({
     Key key,
     @required this.title,
@@ -29,37 +33,91 @@ class Page extends StatefulWidget {
         super(key: key);
 
   final String title;
-  final Future<List<String>> Function() fetch;
-
-  @override
-  _PageState createState() => _PageState();
-}
-
-class _PageState extends State<Page> {
-  Future<List<String>> _links;
-
-  @override
-  void initState() {
-    super.initState();
-    _links = widget.fetch();
-  }
+  final Fetch fetch;
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: Text(widget.title)),
-        body: FutureBuilder<List<String>>(
-          future: _links,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (context, i) =>
-                    ListTile(title: Text(snapshot.data[i])),
-              );
-            }
-
-            return const Center(child: CircularProgressIndicator());
-          },
+        appBar: AppBar(title: Text(title)),
+        body: ChangeNotifierProvider<LinksRepository>(
+          create: (context) => LinksRepository(fetch)..invoke(),
+          child: const LinksListView(),
         ),
       );
 }
+
+class LinksListView extends StatelessWidget {
+  const LinksListView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Consumer<LinksRepository>(
+        builder: (context, repository, child) {
+          if (repository.status == FetchStatus.fetched) {
+            return ListView.builder(
+              itemCount: repository.links.length,
+              itemBuilder: (context, i) =>
+                  ListTile(title: Text(repository.links[i])),
+            );
+          }
+
+          if (repository.status == FetchStatus.failed) {
+            WidgetsBinding.instance
+                .addPostFrameCallback((duration) => Scaffold.of(context)
+                  ..hideCurrentSnackBar()
+                  ..showSnackBar(SnackBar(
+                    content: Text(repository.error.toString()),
+                    action: SnackBarAction(
+                      label: 'Refetch',
+                      onPressed: () => repository.invoke(),
+                    ),
+                  )));
+          }
+
+          return child;
+        },
+        child: const Center(child: CircularProgressIndicator()),
+      );
+}
+
+class LinksRepository extends ChangeNotifier {
+  LinksRepository(this.fetch);
+
+  final Fetch fetch;
+  FetchStatus _status;
+  List<String> _links;
+  FetchException _error;
+
+  FetchStatus get status => _status;
+  List<String> get links => _links;
+  FetchException get error => _error;
+
+  Future<void> invoke() async {
+    if (status == FetchStatus.fetching) {
+      return;
+    }
+
+    _changeStatus(FetchStatus.fetching);
+
+    try {
+      _links = await fetch();
+      _changeStatus(FetchStatus.fetched);
+    } on FetchException catch (e) {
+      _error = e;
+      _changeStatus(FetchStatus.failed);
+    }
+  }
+
+  void _changeStatus(FetchStatus status) {
+    _status = status;
+    notifyListeners();
+  }
+}
+
+typedef Fetch = Future<List<String>> Function();
+
+enum FetchStatus {
+  fetching,
+  fetched,
+  failed,
+}
+
+class FetchException implements Exception {}
