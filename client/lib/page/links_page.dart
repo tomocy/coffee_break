e41e099881c1f9bfd425d9afbe916970ab58b737
tomocy <tomocy.dev@gmail.com>
2020-01-09 19:43:09 +0900

    add Links
    Links represents a list of links.

--- client/lib/page/links_page.dart
+++ client/lib/page/links_page.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
+class Links extends ChangeNotifier {
+  final List<Link> _links = [];
+
+  List<Link> get all => _links;
+  List<Link> get unread => _links.where((link) => !link.read).toList();
+  List<Link> get read => _links.where((link) => link.read).toList();
+
+  void markAsRead(Link link) => _markAsRead(link, true);
+
+  void markAsUnread(Link link) => _markAsRead(link, false);
+
+  void _markAsRead(Link link, bool read) {
+    final i = all.indexOf(link);
+    if (i < 0 || all[i].read == read) {
+      return;
+    }
+
+    all[i].read = read;
+    notifyListeners();
+  }
+}
+
 class Link {
   Link({
     @required this.uri,
