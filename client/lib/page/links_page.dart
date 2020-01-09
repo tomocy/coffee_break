49ecebbbae893299e4eaac1b41461a4bc0bbc1db
tomocy <tomocy.dev@gmail.com>
2020-01-09 20:09:46 +0900

    add ReadLinksPage
    ReadLinksPage shows the read links.

--- client/lib/page/links_page.dart
+++ client/lib/page/links_page.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+class ReadLinksPage extends StatelessWidget {
+  const ReadLinksPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Consumer<Links>(
+      builder: (context, links, child) => LinkListView(links: links.read));
+}
+
 class LinkListView extends StatelessWidget {
   const LinkListView({
     Key key,
