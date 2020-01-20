4d4988a5ae94b207ec2fbe693085f0dcf31158a2
tomocy <tomocy.dev@gmail.com>
2020-01-21 00:54:13 +0900

    update Link
    Link holds its creation time.

--- client/lib/domain/models/link.dart
+++ client/lib/domain/models/link.dart
@@ -3,21 +3,30 @@ final List<Link> initialDataOfListOfLinks = [];
 class Link {
   Link({
     this.uri,
+    DateTime createdAt,
     bool done,
-  }) : isDone = done;
+  })  : createdAt = createdAt ?? DateTime.now(),
+        isDone = done;
 
-  Link.todo({String uri})
-      : this(
+  Link.todo({
+    String uri,
+    DateTime createdAt,
+  }) : this(
           uri: uri,
+          createdAt: createdAt,
           done: false,
         );
 
-  Link.done({String uri})
-      : this(
+  Link.done({
+    String uri,
+    DateTime createdAt,
+  }) : this(
           uri: uri,
+          createdAt: createdAt,
           done: true,
         );
 
   final String uri;
+  final DateTime createdAt;
   bool isDone;
 }
