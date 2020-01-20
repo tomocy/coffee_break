e70f05817af247760fb3f038d339299cc4a2e7c1
tomocy <tomocy.dev@gmail.com>
2020-01-21 02:00:05 +0900

    modify ResourceException

--- client/lib/domain/resources/resource.dart
+++ client/lib/domain/resources/resource.dart
@@ -2,4 +2,7 @@ class ResourceException implements Exception {
   const ResourceException([this.message]);
 
   final String message;
+
+  @override
+  String toString() => message.isNotEmpty ? message : super.toString();
 }
