0dcd43de32d6a11282cd12ee0b44e420e1dcb2c2
tomocy <tomocy.dev@gmail.com>
2020-01-20 17:07:29 +0900

    modify SearchDelegate

--- client/lib/pages/widgets/search_delegate.dart
+++ client/lib/pages/widgets/search_delegate.dart
@@ -2,6 +2,16 @@ import 'package:flutter/material.dart' as material show SearchDelegate;
 import 'package:flutter/material.dart';
 
 abstract class SearchDelegate extends material.SearchDelegate<String> {
+  SearchDelegate({
+    String searchFieldLabel,
+    TextInputType keyboardType,
+    TextInputAction textInputAction,
+  }) : super(
+          searchFieldLabel: searchFieldLabel,
+          keyboardType: keyboardType,
+          textInputAction: textInputAction,
+        );
+
   @override
   ThemeData appBarTheme(BuildContext context) => Theme.of(context);
 
