08421dc83f25d255942cb22caffe06fd24a95c15
tomocy <tomocy.dev@gmail.com>
2020-01-21 01:49:48 +0900

    use ListeningOnSaveLink

--- client/lib/pages/add_link_page.dart
+++ client/lib/pages/add_link_page.dart
@@ -1,6 +1,7 @@
 import 'package:coffee_break/blocs/link_bloc.dart';
 import 'package:coffee_break/domain/models/link.dart';
 import 'package:coffee_break/pages/page.dart';
+import 'package:coffee_break/pages/widgets/listening_on_save_link.dart';
 import 'package:coffee_break/pages/widgets/search_delegate.dart';
 import 'package:flutter/material.dart' hide SearchDelegate;
 import 'package:provider/provider.dart';
@@ -18,41 +19,22 @@ class AddLinkPage extends SearchDelegate {
   Widget buildSuggestions(BuildContext context) => _container;
 
   @override
-  Widget buildResults(BuildContext context) => Consumer<LinkBloc>(
-        builder: (_, bloc, child) => StreamBuilder<bool>(
-            stream: bloc.saved,
-            builder: (context, snapshot) {
-              if (query.isEmpty) {
-                return child;
-              }
+  Widget buildResults(BuildContext context) => ListeningOnSaveLink(
+        onNothing: () {
+          if (query.isEmpty) {
+            return;
+          }
 
-              final link = Link.todo(uri: query);
-              if (!snapshot.hasData && !snapshot.hasError) {
-                bloc.save.add(link);
-                return child;
-              }
-
-              if (snapshot.hasError) {
-                showSnackBar(
-                  context,
-                  SnackBar(
-                    action: SnackBarAction(
-                      onPressed: () => bloc.save.add(link),
-                      label: 'RETRY',
-                    ),
-                    content: Text(snapshot.error.toString()),
-                  ),
-                );
-                return child;
-              }
-
-              WidgetsBinding.instance.addPostFrameCallback((_) {
-                close(context, query);
-                query = '';
-              });
-
-              return child;
-            }),
+          final link = Link.todo(uri: query);
+          Provider.of<LinkBloc>(
+            context,
+            listen: false,
+          ).save.add(link);
+        },
+        onData: (_) => WidgetsBinding.instance.addPostFrameCallback((_) {
+          close(context, query);
+          query = '';
+        }),
         child: _container,
       );
 }
