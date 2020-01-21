4cfd6a9516591d2314e406372413a0d8f5a0b45b
tomocy <tomocy.dev@gmail.com>
2020-01-21 19:10:10 +0900

    modify AppLinkPage

--- client/lib/pages/add_link_page.dart
+++ client/lib/pages/add_link_page.dart
@@ -1,7 +1,5 @@
 import 'package:coffee_break/blocs/link_bloc.dart';
 import 'package:coffee_break/domain/models/link.dart';
-import 'package:coffee_break/pages/page.dart';
-import 'package:coffee_break/pages/widgets/listening_on_save_link.dart';
 import 'package:coffee_break/pages/widgets/search_delegate.dart';
 import 'package:flutter/material.dart' hide SearchDelegate;
 import 'package:provider/provider.dart';
@@ -19,22 +17,22 @@ class AddLinkPage extends SearchDelegate {
   Widget buildSuggestions(BuildContext context) => _container;
 
   @override
-  Widget buildResults(BuildContext context) => ListeningOnSaveLink(
-        onNothing: () {
-          if (query.isEmpty) {
-            return;
-          }
+  Widget buildResults(BuildContext context) {
+    if (query.isEmpty) {
+      return _container;
+    }
 
-          final link = Link.todo(uri: query);
-          Provider.of<LinkBloc>(
-            context,
-            listen: false,
-          ).save.add(link);
-        },
-        onData: (_) => WidgetsBinding.instance.addPostFrameCallback((_) {
-          close(context, query);
-          query = '';
-        }),
-        child: _container,
-      );
+    final link = Link.todo(uri: query);
+    Provider.of<LinkBloc>(
+      context,
+      listen: false,
+    ).save.add(link);
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      close(context, query);
+      query = '';
+    });
+
+    return _container;
+  }
 }
