b6dfc41b4e5fe4b74935af55533059447640df19
tomocy <tomocy.dev@gmail.com>
2020-01-21 20:10:03 +0900

    add MultiStreamErrorHandler

--- client/lib/pages/widgets/stream_error_handlers.dart
+++ client/lib/pages/widgets/stream_error_handlers.dart
@@ -1,9 +1,11 @@
 import 'package:coffee_break/blocs/link_bloc.dart';
 import 'package:coffee_break/domain/models/link.dart';
+import 'package:coffee_break/domain/resources/link_repository.dart';
 import 'package:coffee_break/pages/page.dart';
 import 'package:coffee_break/pages/widgets/retry_snack_bar_action.dart';
 import 'package:coffee_break/pages/widgets/stream_error_handler.dart';
 import 'package:flutter/material.dart';
+import 'package:nested/nested.dart';
 import 'package:provider/provider.dart';
 
 class StreamErrorHandlers extends StatelessWidget {
@@ -16,23 +18,66 @@ class StreamErrorHandlers extends StatelessWidget {
   final Widget child;
 
   @override
-  Widget build(BuildContext context) => StreamErrorHandler<List<Link>>(
-        stream: Provider.of<LinkBloc>(
-          context,
-          listen: false,
-        ).links,
-        onError: (error) => showSnackBar(
-          context,
-          SnackBar(
-            action: RetrySnackBarAction(
-              onPressed: () => Provider.of<LinkBloc>(
-                context,
-                listen: false,
-              ).fetch.add(null),
+  Widget build(BuildContext context) => MultiStreamErrorHandler(
+        handlers: [
+          StreamErrorHandler<List<Link>>(
+            stream: Provider.of<LinkBloc>(
+              context,
+              listen: false,
+            ).links,
+            onError: (error) => showSnackBar(
+              context,
+              SnackBar(
+                action: RetrySnackBarAction(
+                  onPressed: () => Provider.of<LinkBloc>(
+                    context,
+                    listen: false,
+                  ).fetch.add(null),
+                ),
+                content: Text(error.toString()),
+              ),
             ),
-            content: Text(error.toString()),
           ),
-        ),
+          StreamErrorHandler<void>(
+            stream: Provider.of<LinkBloc>(
+              context,
+              listen: false,
+            ).saved,
+            onError: (error) {
+              if (error is! LinkRepositorySaveException) {
+                return;
+              }
+
+              final saveError = error as LinkRepositorySaveException;
+              showSnackBar(
+                context,
+                SnackBar(
+                  action: RetrySnackBarAction(
+                    onPressed: () => Provider.of<LinkBloc>(
+                      context,
+                      listen: false,
+                    ).save.add(saveError.link),
+                  ),
+                  content: Text(error.toString()),
+                ),
+              );
+            },
+          ),
+        ],
         child: child,
       );
+}
+
+class MultiStreamErrorHandler extends Nested {
+  MultiStreamErrorHandler({
+    Key key,
+    @required List<SingleChildWidget> handlers,
+    @required Widget child,
+  })  : assert(handlers != null),
+        assert(child != null),
+        super(
+          key: key,
+          children: handlers,
+          child: child,
+        );
 }
