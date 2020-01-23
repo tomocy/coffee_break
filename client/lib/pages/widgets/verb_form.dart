b8f8e5463a43adc88167622c39aac5837e2f009c
tomocy <tomocy.dev@gmail.com>
2020-01-23 13:46:32 +0900

    move VerbDropdownMenu

--- client/lib/pages/widgets/verb_form.dart
+++ client/lib/pages/widgets/verb_form.dart
@@ -1,6 +1,9 @@
+import 'package:coffee_break/blocs/verb_bloc.dart';
 import 'package:coffee_break/domain/models/verb.dart';
+import 'package:coffee_break/pages/add_verb_page.dart';
 import 'package:coffee_break/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class VerbForm extends StatefulWidget {
   const VerbForm({
@@ -76,4 +79,92 @@ class _VerbFormState extends State<VerbForm> {
           ),
         ),
       );
+}
+
+class VerbDropdownButtonFormField extends StatefulWidget {
+  const VerbDropdownButtonFormField({Key key}) : super(key: key);
+
+  @override
+  _VerbDropdownButtonFormFieldState createState() =>
+      _VerbDropdownButtonFormFieldState();
+}
+
+class _VerbDropdownButtonFormFieldState
+    extends State<VerbDropdownButtonFormField> {
+  Stream<List<Verb>> _verbs;
+  VerbMenuItem _value;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _verbs = Provider.of<VerbBloc>(
+      context,
+      listen: false,
+    ).verbs;
+  }
+
+  @override
+  Widget build(BuildContext context) => StreamBuilder<List<Verb>>(
+        stream: _verbs,
+        builder: (_, snapshot) {
+          if (!snapshot.hasData) {
+            Provider.of<VerbBloc>(
+              context,
+              listen: false,
+            ).notify.add(null);
+            return DropdownButtonFormField<void>(
+              onChanged: (item) {},
+              items: const [],
+            );
+          }
+
+          return DropdownButtonFormField<VerbMenuItem>(
+            isDense: true,
+            value: _value,
+            onChanged: (item) {
+              if (item is _AddVerbMenuItem) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<AddVerbPage>(
+                    builder: (_) => const AddVerbPage(),
+                  ),
+                );
+                return;
+              }
+
+              setState(() => _value = item);
+            },
+            decoration: const InputDecoration(
+              labelText: 'Verb',
+            ),
+            items: snapshot.data
+                .map((verb) => DropdownMenuItem(
+                      value: VerbMenuItem(verb.base),
+                      child: Text(verb.base),
+                    ))
+                .toList()
+                  ..add(const DropdownMenuItem(
+                    value: _AddVerbMenuItem(),
+                    child: Text('Add verb'),
+                  )),
+          );
+        },
+      );
+}
+
+class _AddVerbMenuItem extends VerbMenuItem {
+  const _AddVerbMenuItem() : super('');
+}
+
+class VerbMenuItem {
+  const VerbMenuItem(this.value);
+
+  final String value;
+
+  @override
+  bool operator ==(dynamic other) =>
+      other is VerbMenuItem && other.value == value;
+
+  @override
+  int get hashCode => value.hashCode;
 }
