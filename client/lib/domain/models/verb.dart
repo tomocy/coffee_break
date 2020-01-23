db51a21f7a4786a4e5010f3e4551788cfa027248
tomocy <tomocy.dev@gmail.com>
2020-01-23 16:29:26 +0900

    update Verb

--- client/lib/domain/models/verb.dart
+++ client/lib/domain/models/verb.dart
@@ -4,9 +4,16 @@ class Verb {
   final String base;
   final String pastParticle;
 
-  String get capitalizedBase => base[0].toUpperCase() + base.substring(1);
+  String get capitalizedBase => _capitalize(base);
+
+  String get capitalizedPastParticle => _capitalize(pastParticle);
+
+  String get capitalizedInfinitive => _capitalize(infinitive);
 
   String get infinitive => 'to $base';
+
+  String _capitalize(String target) =>
+      target[0].toUpperCase() + target.substring(1);
 
   @override
   bool operator ==(dynamic other) =>
