3770bd8aebf697760f263fb9febe64338c050625
tomocy <tomocy.dev@gmail.com>
2020-01-23 12:23:13 +0900

    provide VerbBloc

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:coffee_break/app.dart';
 import 'package:coffee_break/blocs/link_bloc.dart';
 import 'package:coffee_break/blocs/settings_bloc.dart';
+import 'package:coffee_break/blocs/verb_bloc.dart';
 import 'package:coffee_break/domain/resources/link_repository.dart';
 import 'package:coffee_break/domain/resources/settings_repository.dart';
+import 'package:coffee_break/domain/resources/verb_repository.dart';
 import 'package:coffee_break/infra/link_repository.dart';
 import 'package:coffee_break/infra/settings_repository.dart';
+import 'package:coffee_break/infra/verb_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -25,6 +28,17 @@ void main() => runApp(MultiProvider(
         ),
         Provider<LinkBloc>(
           create: (context) => LinkBloc(Provider.of<LinkRepository>(
+            context,
+            listen: false,
+          ))
+            ..fetch.add(null),
+          dispose: (_, bloc) => bloc.dispose(),
+        ),
+        Provider<VerbRepository>(
+          create: (_) => MockVerbRepository(),
+        ),
+        Provider<VerbBloc>(
+          create: (context) => VerbBloc(Provider.of<VerbRepository>(
             context,
             listen: false,
           ))
