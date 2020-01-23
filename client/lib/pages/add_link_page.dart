1f8357a95a276e29b136b31cebc651e0c0f58d00
tomocy <tomocy.dev@gmail.com>
2020-01-23 14:24:51 +0900

    modify AddLinkPage
    AddLinkPage handles only the stream of its children.

--- client/lib/pages/add_link_page.dart
+++ client/lib/pages/add_link_page.dart
@@ -1,5 +1,12 @@
 import 'package:coffee_break/blocs/link_bloc.dart';
+import 'package:coffee_break/blocs/verb_bloc.dart';
+import 'package:coffee_break/domain/models/verb.dart';
+import 'package:coffee_break/domain/resources/verb_repository.dart';
+import 'package:coffee_break/pages/page.dart';
 import 'package:coffee_break/pages/widgets/link_form.dart';
+import 'package:coffee_break/pages/widgets/retry_snack_bar_action.dart';
+import 'package:coffee_break/pages/widgets/stream_handler.dart';
+import 'package:coffee_break/pages/widgets/stream_handlers.dart';
 import 'package:flutter/material.dart' hide SearchDelegate;
 import 'package:provider/provider.dart';
 
@@ -9,18 +16,52 @@ class AddLinkPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(title: const Text('Add link')),
-        body: SafeArea(
-          child: LinkForm(
-            onSubmit: (link) {
-              Provider.of<LinkBloc>(
-                context,
-                listen: false,
-              ).save.add(link);
+        body: _buildStreamHandlers(
+          context,
+          SafeArea(
+            child: LinkForm(
+              onSubmit: (link) {
+                Provider.of<LinkBloc>(
+                  context,
+                  listen: false,
+                ).save.add(link);
 
-              Navigator.pop(context);
-            },
-            submitButtonLabel: 'Add',
+                Navigator.pop(context);
+              },
+              submitButtonLabel: 'Add',
+            ),
           ),
         ),
+      );
+
+  Widget _buildStreamHandlers(BuildContext context, Widget child) =>
+      MultiStreamHandler(
+        handlers: [
+          StreamHandler<List<Verb>>(
+            stream: Provider.of<VerbBloc>(
+              context,
+              listen: false,
+            ).verbs,
+            onError: (_, error) {
+              if (error is! VerbRepositoryFetchException) {
+                return;
+              }
+
+              showSnackBar(
+                context,
+                SnackBar(
+                  action: RetrySnackBarAction(
+                    onPressed: () => Provider.of<VerbBloc>(
+                      context,
+                      listen: false,
+                    ).fetch.add(null),
+                  ),
+                  content: Text(error.toString()),
+                ),
+              );
+            },
+          ),
+        ],
+        child: child,
       );
 }
