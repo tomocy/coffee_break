4a3467166c096c1797f1d81536a7a87d35fe849f
tomocy <tomocy.dev@gmail.com>
2020-01-23 13:18:14 +0900

    add StreamdLinksListView

---
+++ client/lib/pages/widgets/streamed_links_list_view.dart
@@ -0,0 +1,45 @@
+import 'package:coffee_break/blocs/link_bloc.dart';
+import 'package:coffee_break/domain/models/link.dart';
+import 'package:coffee_break/pages/widgets/link_tile.dart';
+import 'package:coffee_break/pages/widgets/refreshable.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class StreamedLinksListView extends StatelessWidget {
+  const StreamedLinksListView({Key key, @required this.stream})
+      : assert(stream != null),
+        super(key: key);
+
+  final Stream<List<Link>> Function(LinkBloc) stream;
+
+  @override
+  Widget build(BuildContext context) => Consumer<LinkBloc>(
+        builder: (_, bloc, child) => StreamBuilder<List<Link>>(
+          stream: stream(bloc),
+          builder: (_, snapshot) {
+            if (!snapshot.hasData && !snapshot.hasError) {
+              bloc.notify.add(null);
+              return child;
+            }
+            if (snapshot.hasError) {
+              return child;
+            }
+
+            return _buildFetchableLinkListView(context, snapshot.data);
+          },
+        ),
+        child: _buildFetchableLinkListView(context),
+      );
+
+  Widget _buildFetchableLinkListView(
+    BuildContext context, [
+    List<Link> links = const [],
+  ]) =>
+      Refreshable(
+        onRefresh: () async => Provider.of<LinkBloc>(
+          context,
+          listen: false,
+        ).fetch.add(null),
+        child: buildLinkListView(links),
+      );
+}
