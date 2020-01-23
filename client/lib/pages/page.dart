bd11e4e318b0b875acad12f22a61259b94f6946d
tomocy <tomocy.dev@gmail.com>
2020-01-23 14:22:39 +0900

    modify Page
    Page handles the only stream of its children.

--- client/lib/pages/page.dart
+++ client/lib/pages/page.dart
@@ -1,7 +1,14 @@
+import 'package:coffee_break/blocs/link_bloc.dart';
+import 'package:coffee_break/domain/models/link.dart';
+import 'package:coffee_break/domain/resources/link_repository.dart';
 import 'package:coffee_break/pages/add_link_page.dart';
 import 'package:coffee_break/pages/search_links_page.dart';
+import 'package:coffee_break/pages/widgets/retry_snack_bar_action.dart';
+import 'package:coffee_break/pages/widgets/stream_handler.dart';
 import 'package:coffee_break/pages/widgets/stream_handlers.dart';
+import 'package:coffee_break/pages/widgets/undo_snack_bar_action.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 const routes = <PageType, String>{
   PageType.todo: '/todo',
@@ -111,7 +118,97 @@ class Page extends StatelessWidget {
           ),
         ),
         drawerScrimColor: Colors.transparent,
-        body: StreamHandlers(child: SafeArea(child: body)),
+        body: _buildStreamHandlers(
+          context,
+          SafeArea(child: body),
+        ),
+      );
+
+  Widget _buildStreamHandlers(BuildContext context, Widget child) =>
+      MultiStreamHandler(
+        handlers: [
+          StreamHandler<List<Link>>(
+            stream: Provider.of<LinkBloc>(
+              context,
+              listen: false,
+            ).links,
+            onError: (_, error) => showSnackBar(
+              context,
+              SnackBar(
+                action: RetrySnackBarAction(
+                  onPressed: () => Provider.of<LinkBloc>(
+                    context,
+                    listen: false,
+                  ).fetch.add(null),
+                ),
+                content: Text(error.toString()),
+              ),
+            ),
+          ),
+          StreamHandler<void>(
+            stream: Provider.of<LinkBloc>(
+              context,
+              listen: false,
+            ).saved,
+            onError: (_, error) {
+              if (error is! LinkRepositorySaveException) {
+                return;
+              }
+
+              final saveError = error as LinkRepositorySaveException;
+              showSnackBar(
+                context,
+                SnackBar(
+                  action: RetrySnackBarAction(
+                    onPressed: () => Provider.of<LinkBloc>(
+                      context,
+                      listen: false,
+                    ).save.add(saveError.link),
+                  ),
+                  content: Text(error.toString()),
+                ),
+              );
+            },
+          ),
+          StreamHandler<Link>(
+            stream: Provider.of<LinkBloc>(
+              context,
+              listen: false,
+            ).deleted,
+            onData: (context, link) => showSnackBar(
+              context,
+              SnackBar(
+                action: UndoSnackBarAction(
+                  onPressed: () => Provider.of<LinkBloc>(
+                    context,
+                    listen: false,
+                  ).save.add(link),
+                ),
+                content: const Text('Link was deleted.'),
+              ),
+            ),
+            onError: (_, error) {
+              if (error is! LinkRepositoryDeleteException) {
+                return;
+              }
+
+              final deleteError = error as LinkRepositoryDeleteException;
+              showSnackBar(
+                context,
+                SnackBar(
+                  action: RetrySnackBarAction(
+                    onPressed: () => Provider.of<LinkBloc>(
+                      context,
+                      listen: false,
+                    ).delete.add(deleteError.link),
+                  ),
+                  content: Text(error.toString()),
+                ),
+              );
+            },
+          ),
+        ],
+        child: child,
       );
 }
 
