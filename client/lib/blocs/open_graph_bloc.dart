cebb52bd7d65ebb090171c3acd87e1aaeaae63d7
tomocy <tomocy.dev@gmail.com>
2020-01-29 13:20:59 +0900

    add OpenGraphBloc

---
+++ client/lib/blocs/open_graph_bloc.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import 'package:coffee_break/domain/models/open_graph.dart';
+import 'package:coffee_break/domain/resources/open_graph_repository.dart';
+
+class OpenGraphBloc {
+  OpenGraphBloc(this._repository) {
+    _fetchController.stream.listen(_invokeFetch);
+  }
+
+  final OpenGraphRepository _repository;
+  final _openGraphController = StreamController<OpenGraph>.broadcast();
+  final _fetchController = StreamController<String>();
+
+  Stream<OpenGraph> get openGraph => _openGraphController.stream;
+
+  Sink<String> get fetch => _fetchController.sink;
+
+  Future<void> _invokeFetch(String uri) async {
+    try {
+      final openGraph = await _repository.fetch(uri);
+      _openGraphController.add(openGraph);
+    } on OpenGraphRepositoryFetchException catch (e) {
+      _openGraphController.addError(e);
+    }
+  }
+
+  void dispose() {
+    _openGraphController.close();
+    _fetchController.close();
+  }
+}
