a40722db366d738b140b68a89e739c0f1cda3453
tomocy <tomocy.dev@gmail.com>
2020-01-29 13:33:58 +0900

    fix

--- client/lib/domain/resources/open_graph_repository.dart
+++ client/lib/domain/resources/open_graph_repository.dart
@@ -1,6 +1,7 @@
 import 'package:coffee_break/domain/models/open_graph.dart';
 import 'package:coffee_break/domain/resources/resource.dart';
 
+// ignore: one_member_abstracts
 abstract class OpenGraphRepository {
   Future<OpenGraph> fetch(String uri);
 }
