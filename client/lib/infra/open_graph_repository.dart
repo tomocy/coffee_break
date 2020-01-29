c2e955ebb10c28d488fbebece0206bfea6229ec7
tomocy <tomocy.dev@gmail.com>
2020-01-29 13:14:58 +0900

    fix

--- client/lib/infra/open_graph_repository.dart
+++ client/lib/infra/open_graph_repository.dart
@@ -1,12 +1,17 @@
 import 'package:coffee_break/domain/models/open_graph.dart';
 import 'package:coffee_break/domain/resources/open_graph_repository.dart';
+import 'package:coffee_break/infra/mock.dart';
 
-class MockOpenGraphRepository implements OpenGraphRepository {
+class MockOpenGraphRepository extends Mock implements OpenGraphRepository {
   final _openGraphs = <String, OpenGraph>{
     'https://github.com': const OpenGraph(title: 'GitHub'),
     'https://twitter.com':
         const OpenGraph(title: "Twitter. It's what's happening."),
   };
 
-  Future<OpenGraph> fetch(String uri) async => _openGraphs[uri];
+  @override
+  Future<OpenGraph> fetch(String uri) async => !doFail
+      ? _openGraphs[uri]
+      : throw const OpenGraphRepositoryFetchException(
+          'failed to fetch open graph');
 }
