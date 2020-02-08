bc2853683038e445d249a8d1d74806b2a5190137
tomocy <tomocy.dev@gmail.com>
2020-02-08 23:40:21 +0900

    add test for SettingsBloc
    This test is for SettingsBloc.save.

--- client/test/unit/blocs/settings_bloc_test.dart
+++ client/test/unit/blocs/settings_bloc_test.dart
@@ -1,6 +1,7 @@
 import 'package:coffee_break/blocs/settings_bloc.dart';
 import 'package:coffee_break/infra/settings_repository.dart';
 import 'package:coffee_break/domain/models/settings.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() => group('SettingsBloc test', () {
@@ -11,7 +12,10 @@ void main() => group('SettingsBloc test', () {
           final bloc = SettingsBloc(repository);
 
           bloc.fetch.add(null);
-          await expectLater(bloc.settings, emits(expected));
+          await expectLater(
+            bloc.settings,
+            emits(expected),
+          );
           bloc.dispose();
         });
 
@@ -21,6 +25,34 @@ void main() => group('SettingsBloc test', () {
           final bloc = SettingsBloc(repository);
 
           bloc.fetch.add(null);
+          await expectLater(
+            bloc.settings,
+            emits(expected),
+          );
+          bloc.dispose();
+        });
+      });
+
+      group('save', () {
+        test('success', () async {
+          final expected = Settings(themeMode: ThemeMode.dark);
+          final repository = MockSettingsRepository();
+          final bloc = SettingsBloc(repository);
+
+          bloc.save.add(expected);
+          await expectLater(
+            bloc.settings,
+            emits(expected),
+          );
+          bloc.dispose();
+        });
+
+        test('failed', () async {
+          final expected = Settings(themeMode: ThemeMode.dark);
+          final repository = MockSettingsRepository(failer: () => true);
+          final bloc = SettingsBloc(repository);
+
+          bloc.save.add(expected);
           await expectLater(
             bloc.settings,
             emits(expected),
