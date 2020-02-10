19c384ffa54a6feba70c6f43bc225bcdc322c330
tomocy <tomocy.dev@gmail.com>
2020-02-10 23:42:40 +0900

    add test for OpenGraphBloc
    This test is for OpenGraphBloc.fetch.

---
+++ client/test/unit/blocs/open_graph_bloc_test.dart
@@ -0,0 +1,40 @@
+import 'package:coffee_break/blocs/open_graph_bloc.dart';
+import 'package:coffee_break/domain/models/open_graph.dart';
+import 'package:coffee_break/domain/resources/open_graph_repository.dart';
+import 'package:coffee_break/infra/open_graph_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() => group('OpenGraphBloc test', () {
+      group('fetch', () {
+        test('success', () async {
+          const expected = OpenGraph(
+            title: 'test open graph',
+          );
+          const repository = MockOpenGraphRepository(
+            openGraphs: {'test': expected},
+          );
+          final bloc = OpenGraphBloc(repository);
+
+          bloc.fetch.add('test');
+          await expectLater(
+            bloc.openGraph,
+            emits(expected),
+          );
+          bloc.dispose();
+        });
+
+        test('failed', () async {
+          final repository = MockOpenGraphRepository(
+            failer: () => true,
+          );
+          final bloc = OpenGraphBloc(repository);
+
+          bloc.fetch.add('');
+          await expectLater(
+            bloc.openGraph,
+            emitsError(const OpenGraphRepositoryFetchException()),
+          );
+          bloc.dispose();
+        });
+      });
+    });
