ba316a5e1a697099f2b74ace8719efaf64fafbb8
tomocy <tomocy.dev@gmail.com>
2020-02-11 22:46:51 +0900

    add test for LinkBloc
    This test is for LinkBloc.fetch.

---
+++ client/test/unit/blocs/link_bloc_test.dart
@@ -0,0 +1,51 @@
+import 'package:coffee_break/blocs/link_bloc.dart';
+import 'package:coffee_break/domain/models/link.dart';
+import 'package:coffee_break/domain/resources/link_repository.dart';
+import 'package:coffee_break/infra/link_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() => group('LinkBloc test', () {
+      group('fetch', () {
+        test('success', () async {
+          final expected = <Link>[
+            Link.todo(
+              uri: 'uri 1',
+              title: 'title 1',
+            ),
+            Link.todo(
+              uri: 'uri 2',
+              title: 'title 2',
+            ),
+            Link.done(
+              uri: 'uri 3',
+              title: 'title 3',
+            ),
+          ];
+          final repository = MockLinkRepository(
+            links: expected,
+          );
+          final bloc = LinkBloc(repository);
+
+          bloc.fetch.add(null);
+          await expectLater(
+            bloc.links,
+            emits(expected),
+          );
+          bloc.dispose();
+        });
+
+        test('failed', () async {
+          final repository = MockLinkRepository(
+            failer: () => true,
+          );
+          final bloc = LinkBloc(repository);
+
+          bloc.fetch.add(null);
+          await expectLater(
+            bloc.links,
+            emitsError(const LinkRepositoryFetchException()),
+          );
+          bloc.dispose();
+        });
+      });
+    });
